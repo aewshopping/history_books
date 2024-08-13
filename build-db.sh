1b1156b1c9d5bd565a46f66dcbb3564058611c9c
aewshopping <aewshopping@gmail.com>
2024-08-13 18:58:58 +0100

    Update build-db.sh

--- build-db.sh
+++ build-db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sqlite-utils insert data.db books 'data_csv/popular-history-books.csv' --csv --pk isbn_10 File
-sqlite-utils insert data.db books_tags 'data_csv/books-tags.csv' --csv File
-sqlite-utils insert data.db tags 'data_csv/tags.csv' --csv --pk pk_tag_id File
-sqlite-utils transform data.db tags --type tag_sort INTEGER
-sqlite-utils insert data.db cats 'data_csv/cats.csv' --csv --pk pk_cat_id File
-sqlite-utils transform data.db cats --type cat_sort INTEGER
-sqlite-utils enable-fts data.db books title author publisher --fts4 --tokenize porter
-sqlite-utils transform data.db books --type pages INTEGER
-sqlite-utils convert data.db books hb_publish_date 'r.parsedate(value, dayfirst=True)'
+sqlite-utils insert hbooks.db books 'data_csv/popular-history-books.csv' --csv --pk isbn_10 File
+sqlite-utils insert hbooks.db books_tags 'data_csv/books-tags.csv' --csv File
+sqlite-utils insert hbooks.db tags 'data_csv/tags.csv' --csv --pk pk_tag_id File
+sqlite-utils transform hbooks.db tags --type tag_sort INTEGER
+sqlite-utils insert hbooks.db cats 'data_csv/cats.csv' --csv --pk pk_cat_id File
+sqlite-utils transform hbooks.db cats --type cat_sort INTEGER
+sqlite-utils enable-fts hbooks.db books title author publisher --fts4 --tokenize porter
+sqlite-utils transform hbooks.db books --type pages INTEGER
+sqlite-utils convert hbooks.db books hb_publish_date 'r.parsedate(value, dayfirst=True)'
